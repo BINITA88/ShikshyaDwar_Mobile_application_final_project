1f42bba7bb0781eed8938ee1d6e599853332cbae
Binita <binitaacharya2004@gmail.com>
2025-01-07 18:05:56 +0545

    Save progress on home_view.dart

---
+++ lib/features/home/presentation/view/home_view.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shikshyadwar_mobile_application_project/core/common/snackbar/my_snackbar.dart';
+import 'package:shikshyadwar_mobile_application_project/features/home/presentation/view/bottom_view/dashboard_view.dart';
+import 'package:shikshyadwar_mobile_application_project/features/home/presentation/view_model/home_cubit.dart';
+import 'package:shikshyadwar_mobile_application_project/features/home/presentation/view_model/home_state.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              showMySnackBar(
+                context: context,
+                message: 'Logging out...',
+                color: Colors.red,
+              );
+              // Handle logout logic here
+            },
+          ),
+        ],
+      ),
+      body: BlocBuilder<HomeCubit, HomeState>(
+        builder: (context, state) {
+          return state.selectedIndex == 0
+              ? DashboardView() // Show DashboardView as default
+              : state
+                  .views[state.selectedIndex]; // Dynamic content for other tabs
+        },
+      ),
+// bottom 
+      // Bottom Navigation Bar
+      bottomNavigationBar: BlocBuilder<HomeCubit, HomeState>(
+        builder: (context, state) {
+          return BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.dashboard),
+                label: 'Dashboard',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'Course',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.group),
+                label: 'Batch',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                label: 'Account',
+              ),
+            ],
+            currentIndex: state.selectedIndex,
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            onTap: (index) {
+              context.read<HomeCubit>().onTabTapped(index);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
